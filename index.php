c1a0c8c9ceaf6e0dd7d535adad4d99a044149083
tony <tony@tony-LIFEBOOK-A544>
2018-08-16 23:39:15 +0200

    inserimento nuovi file automatici

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+// pdgt-esercitazione-heroku
+
+//per gestire corpo richiesta, si legge il contenuto della richiesta
+$content = file_get_contents("php://input");
+
+	if(!$content)
+		exit();
+	
+	$update = json_decode($content, false);//si ottiene oggetto php
+	
+	$message = $update->message;//update è tutto il blocco, message il pezzo
+	
+	$message_id = $message->message_id;
+	$text = $message->text;
+	
+	                        //graffe, dollaro e variabile, variabili php dentro stringa
+	error_log("Message ID {$message_id}: {$text}\n");  //usa php per logare errori di sistema
+//il bot invia mess, viene decodificato e scompattato
+
+//meglio non chiudere perche php stampa tutto
+	$chat_id = $message->chat->id;
+	//$token = "qui si metterebbe il token telegram ma non è sicuro";
+	//il runtime ci da il codice token, lo otteniamo da fuori, getenv sono variabili che eseguono il nostro codice
+	$token = getenv("BOTTOKEN");
+
+////////////////////////
+
+//aggiunto da controllare funzionamento
+
+$name = $dati->result[0]->message->from->first_name;
+
+
+	$ora = date('H:i');
+	$giorno = date('d/m/Y');
+	
+	
+$message = "\nBenvenuto ".$name."!".", sono le: $ora, del giorno: $giorno"; 
+
+echo $message; 
+ 
+
+///////////////////////////////
+
+	
+	$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" . urlencode("\nMi hai scritto questo: {$text}");
+//stringa convertita per inserire nell'url per essere compattibile
+
+error_log("URL: " . $url);
+
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_POST, true);
+$response = curl_exec($handle);
+
+error_log("sendMessage: " . $response);
