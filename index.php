6f485e10f9947c7d6ee19719682aea714f776e46
pedro <40278012+botaltervista@users.noreply.github.com>
2018-09-23 21:36:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -38,14 +38,17 @@ $token = getenv("BOTTOKEN");
 $message = "\nCiao,".$name." sono le: $ora, del giorno: $giorno";  
 
 
-/////////
-//$tastiera = '&reply_markup={"keyboard":[["Tastiera%20inline"],["Nascondi%20Tastiera","Rimuovi%20Tastiera"]]"one_time_keyboard":true}';
-/////////
-
 ///////////////////////////////
 
-$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" . urlencode("$message \nMi hai scritto questo: {$text}");
+//$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" . urlencode("$message \nMi hai scritto questo: {$text}");
 //stringa convertita per inserire nell'url per essere compattibile
+
+
+ $tastiera = '&reply_markup={"keyboard":[["Tastiera%20inline"],["Nascondi%20Tastiera","Rimuovi%20Tastiera"]]"one_time_keyboard":true}';
+ $url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" .urlencode($text).$tastiera;
+ 
+ //$url = $GLOBALS[website]."sendMessage?chat_id="{$chat_id}"&text=".urlencode($text).$tastiera;
+ file_get_contents($url);
 
 
 
