ae879ff1b258bf063fdca5cbcdae0b0e1c9a8694
pedro <40278012+botaltervista@users.noreply.github.com>
2018-09-25 21:11:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -54,6 +54,64 @@ while($Riga=mysql_fetch_assoc($Search))
 
 //////////
 
+
+//****************************
+
+/**
+ * Creates or retrieves a connection to the database.
+ * @param bool $quick True if the connection should be returned untested.
+ * @return object A valid connection to the database.
+ */
+function db_open_connection($quick = false) {
+    if(isset($GLOBALS['db_connection'])) {
+        $connection = $GLOBALS['db_connection'];
+
+        if(!$quick) {
+            // Ping the connection just to be safe
+            // This can be removed for performance since we usually have no
+            // long-running scripts.
+            if(!mysqli_ping($connection)) {
+                error_log('Database connection already open but does not respond to ping');
+                die();
+            }
+        }
+
+        return $connection;
+    }
+    else {
+        // Check configuration
+        if(!DATABASE_USERNAME || !DATABASE_NAME) {
+            error_log('Please configure the database connection in file config.php');
+            die();
+        }
+
+        // Open up a new connection
+        $connection = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME);
+
+        if(!$connection) {
+            $errno = mysqli_connect_errno();
+            $error = mysqli_connect_error();
+            error_log("Failed to establish database connection. Error #$errno: $error");
+            die();
+        }
+
+        // Store connection for later
+        $GLOBALS['db_connection'] = $connection;
+
+        // Register clean up function for termination
+        register_shutdown_function('db_close_connection');
+
+        return $connection;
+    }
+}
+
+//****************************
+
+
+
+
+
+
 ////////////////////////
 
 //aggiunto da controllare funzionamento
