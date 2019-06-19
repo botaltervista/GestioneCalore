ab2d7ef9d88eb25a32fa817fdba9913f7212457f
pedro <40278012+botaltervista@users.noreply.github.com>
2019-06-19 18:35:59 +0200

    Update index.php
    mensaje

--- index.php
+++ index.php
@@ -108,8 +108,8 @@ break;
 	       
 	//http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$mensaje);
 	
-
-$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" . urlencode("$mensaje\n");        
+//$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" . urlencode("$message1 $message\nOggi mi hai scritto questo: {$text}");
+  $url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" . urlencode("$mensaje");        
         
         	
 	}
