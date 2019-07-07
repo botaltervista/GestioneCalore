e21b94f83f141d8d999129451196a4f9767b10e6
pedro <40278012+botaltervista@users.noreply.github.com>
2019-07-07 22:18:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ else{
 	$token = getenv("BOTTOKEN");
 
 	if($text === '/start'){	
-		$avviso = 'Benvenuto nel servizio di messagistica sulla Gestione Calore';
+		$avviso = 'Benvenuto nel servizio di messaggistica sulla Gestione Calore';
 		
 		http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$avviso);
 	}
