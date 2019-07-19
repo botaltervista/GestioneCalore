5033b9e8b79ffebe5c1673794d71fe8c7c74787c
pedro <40278012+botaltervista@users.noreply.github.com>
2019-07-19 11:00:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -86,6 +86,9 @@ else{
 	
 	//se viene inserita la parola /impianti
 	else if($text === '/7'){
+		$avviso = 'Elenco degli impianti in servizio attualmente:';
+		
+		http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$avviso);		
 		impianti();
 	
 	}
