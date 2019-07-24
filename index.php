5478485b16b04de9cd12dba9572f6681c9221ff5
pedro <40278012+botaltervista@users.noreply.github.com>
2019-07-24 23:20:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -129,7 +129,7 @@ else{
 	   		}//fine foreach
 		  
    		for($xx = 0; $xx <= $cn; $xx = $xx + 1){
-				if($scelta === 'K011'){
+				if($scelta === $datos[$xx]){
 					$avviso = 'impianto trovato';
 					http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$avviso.$scelta);
 				}
