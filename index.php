0fe376782c00ee7220e082f233e08f61a1d59c72
pedro <40278012+botaltervista@users.noreply.github.com>
2019-07-28 17:14:21 +0200

    Update index.php
    scelta == datos

--- index.php
+++ index.php
@@ -165,7 +165,7 @@ else{
 		http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$messaggio.$cn.$datos[$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn]);
 		
    		for($xx = 0; $xx <= $cn; $xx = $xx + 1){
-				if($scelta === $datos[$xx]){
+				if($scelta == $datos[$xx]){
 					$avviso = 'impianto trovato';
 					http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$avviso.$scelta);
 				}
