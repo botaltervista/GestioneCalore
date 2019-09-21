5a7295cf444f70bc91cc8dfa7d860709fe117b84
pedro <40278012+botaltervista@users.noreply.github.com>
2019-09-22 01:30:13 +0200

    Update index.php
    menu dentro scelta 2

--- index.php
+++ index.php
@@ -230,7 +230,7 @@ else{
   			$xx = $xx + 1;
 		}//fine confronto impianti con foreach
 		
-		http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$datos[$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn]);
+		//http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text=".$datos[$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn][$cn]);
 		
 		
 		$avviso = 'Menu del servizio di messaggistica sulla Gestione Calore scelga una opzione:';
